e8c3d89965c7a21c6d003674e25a0a5c03e673ff
Briant--S <briantsandriano@gmail.com>
2025-05-09 15:21:00 +0700

    nuri navbar update 0.1.1

--- nuri/nuri.js
+++ nuri/nuri.js
@@ -1,3 +1,9 @@
 function sidebar_open() {
-    document.
+    document.getElementById("closed-sidebar").style.display = "none";
+    document.getElementById("opened-sidebar").style.display = "flex";
+}
+
+function sidebar_close(){
+    document.getElementById("closed-sidebar").style.display = "flex";
+    document.getElementById("opened-sidebar").style.display = "none";
 }
