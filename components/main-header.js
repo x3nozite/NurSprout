983468875799aec9a049fa8f54091f16447a1dd4
Briant--S <briantsandriano@gmail.com>
2025-05-14 08:05:30 +0700

    benerin header

--- components/main-header.js
+++ components/main-header.js
@@ -10,14 +10,14 @@ class Header extends HTMLElement{
         <div class="page-header">
             <div class="header-logo">
                 <img src="../assets/logo.png" alt="NurSprout Logo">
-                <a href="">NurSprout</a>
+                <a href="../index.html">NurSprout</a>
             </div>
             <div class="menu-link">
-                <a href="../home.html">home</a>
-                <a href="../nuri/nuri.html">nuri</a>
+                <a href="../home.html">Home</a>
+                <a href="../nuri/nuri.html">NuRI</a>
             </div>
             <div class="account-button">
-                <a href="/registration/register.html">Sign Up</a>
+                <a href="/registration/register.html" id="sign-up">Sign Up</a>
                 <a href="/registration/login.html" id="login">Login</a>
             </div>
         </div>
