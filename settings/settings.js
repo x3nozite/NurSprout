ba0a88c6ee8fa066759498559d2d842ed927e6b5
x3nozite <xeno.zite3@gmail.com>
2025-06-02 19:46:55 +0700

    salvaging what i can

---
+++ settings/settings.js
@@ -0,0 +1,8 @@
+if(!localStorage.getItem("login-status")){
+  alert("You must be logged in to access the home page!");
+  window.location.href = "/registration/register.html";
+}
+
+const displayName = localStorage.getItem("username");
+
+document.getElementById("display-name").innerHTML = displayName;
