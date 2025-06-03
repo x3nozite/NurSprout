a581dcc9e2e8274df36171126ec351bce88faebb
CodeWCBrandon <christian.brandon@binus.ac.id>
2025-06-03 15:13:50 +0700

    adding local storage for saving the sleep schedule

--- sleeptime.js
+++ sleeptime.js
@@ -1,8 +1,20 @@
 //slider
 const slider = document.querySelector("#slider");
 
-noUiSlider.create(slider, {
-  start: [21, 30],
+let first = 21;
+let second = 30;
+
+// Restore saved value
+const savedValues = JSON.parse(localStorage.getItem("sleepSchedule"));
+if (savedValues && Array.isArray(savedValues)) 
+{
+  first = parseInt(savedValues[0]);
+  second = parseInt(savedValues[1]);
+}
+
+noUiSlider.create(slider, 
+{
+  start: [first, second],
   connect: true,
   range: {
     min: 17,
@@ -23,11 +35,15 @@ noUiSlider.create(slider, {
   }
 });
 
+
 const confirm = document.querySelector("#confirm-button");
 const back = document.querySelector("#back-button");
 
 confirm.addEventListener("click", () => 
 {
+  const sliderValue = slider.noUiSlider.get(true);
+  localStorage.setItem("sleepSchedule", JSON.stringify(sliderValue));
+
   alert("Sleep Schedule updated");
   window.location.href = "home.html";
 });
