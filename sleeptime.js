8b86062d46d4c7de86787e7be20872661f0159ac
CodeWCBrandon <christian.brandon@binus.ac.id>
2025-06-03 14:57:48 +0700

    adding button functionality

--- sleeptime.js
+++ sleeptime.js
@@ -1,7 +1,8 @@
+//slider
 const slider = document.querySelector("#slider");
 
 noUiSlider.create(slider, {
-  start: [21, 30], // 5PM to 10AM next day
+  start: [21, 30],
   connect: true,
   range: {
     min: 17,
@@ -9,8 +10,10 @@ noUiSlider.create(slider, {
   },
   step: 1,
   tooltips: [true, true],
-  format: {
-    to: value => {
+  format: 
+  {
+    to: value => 
+    {
       const hour = Math.floor(value % 24);
       const ampm = hour >= 12 ? 'pm' : 'am';
       const hour12 = hour % 12 === 0 ? 12 : hour % 12;
@@ -18,4 +21,18 @@ noUiSlider.create(slider, {
     },
     from: value => parseInt(value)
   }
+});
+
+const confirm = document.querySelector("#confirm-button");
+const back = document.querySelector("#back-button");
+
+confirm.addEventListener("click", () => 
+{
+  alert("Sleep Schedule updated");
+  window.location.href = "home.html";
+});
+
+back.addEventListener("click", () => 
+{ 
+  window.location.href = "home.html "
 });
